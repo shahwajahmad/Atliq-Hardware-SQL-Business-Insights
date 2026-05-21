68fcde5b5e351a09f066ff5f07bf650acfb17a7d
Shahwaj Ahmad <158546201+shahwajahmad@users.noreply.github.com>
2026-05-22 02:29:00 +0530

    Create Request-3-A-SQL.sql

---
+++ SQL Queries/Ad-Hoc Requests/Request-1/Request-3-A-SQL.sql
@@ -0,0 +1,7 @@
+#  Provide a report with all the unique product counts for each  segment  and sort them in descending order of product counts. The final output contains 
+SELECT 
+      segment ,
+      count(distinct product_code) as product_count
+FROM gdb023.dim_product
+group by segment
+order by 2 desc
